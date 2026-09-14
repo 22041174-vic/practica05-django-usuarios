f97bd2f9a5e1503b7ae6d7938f2e1f0063233972
22041174-vic <22041174@itdurango.edu.mx>
2026-09-13 22:45:38 -0600

    Add files via upload

--- practica05_DyangoMVT/settings.py
+++ practica05_DyangoMVT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-najj%x41m5p40$#(xnn8s)jg@v!-%k^8h81ta3vsp1kqy2+ckb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
